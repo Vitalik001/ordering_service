83d9f93189bcfc4ba81831f7654d3d0512b52756
Vitalik <91615314+Vitalik001@users.noreply.github.com>
2023-08-14 18:15:09 +0300

    added endpoints

---
+++ models.py
@@ -0,0 +1,30 @@
+import uuid
+from typing import List
+
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class Item(BaseModel):
+    id: int = uuid.uuid4()
+    name: str
+    price: float
+    number: int
+
+
+class Stats(BaseModel):
+    total_orders: int
+    total_order_price: float
+    avg_order_price: float
+    total_items: int
+    avg_items: float
+    most_ordered_item: str
+
+
+class Order(BaseModel):
+    id: int = uuid.uuid4()
+    created_date: datetime
+    updated_date: datetime
+    title: str
+    total: float
+    items: List[Item]
